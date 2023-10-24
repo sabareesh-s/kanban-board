086328ef12ee294c3414a49af59c21d485a23998
sabareesh-s <sabareesh1221@gmail.com>
2023-10-24 17:59:31 +0530

    styling changes - adding breakpoint prefixes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import KanbanBoard from './components/KanbanBoard';
 
 function App() {
   return (
-    <main className="w-screen h-screen flex justify-center items-start pt-16 bg-[url('https://i.imgur.com/QzikZzo.jpg')] bg-cover">
+    <main className="md:w-screen h-screen flex md:justify-center items-start pt-16 p-4 md:bg-[url('https://i.imgur.com/QzikZzo.jpg')] bg-cover">
       {/* <h1 className="font-semibold text-2xl">Kanban Board</h1> */}
       <section className='max-w-[840px] flex-shrink-0 p-4 bg-white rounded-xl shadow-md border backdrop-blur-sm'>
         <KanbanBoard />
